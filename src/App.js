dc32fb23ba010c397ecd56d817c7b754768fc611
dormedina111 <110923343+dormedina111@users.noreply.github.com>
2024-05-23 20:59:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Lobby from './components/Lobby';
 import CodeBlockPage from './components/CodeBlockPage';
+const url = process.env.REACT_APP_URL;
 
 function App() {
     const [codeBlocks, setCodeBlocks] = useState([]);
+    
 
     useEffect(() => {
         // Fetch code-blocks from the server
-        fetch('http://localhost:5000/api/code-blocks')
+        fetch(`${url}/api/code-blocks`)
             .then(response => response.json())
             .then(data => setCodeBlocks(data))
             .catch(error => console.error('Error fetching code blocks:', error));
